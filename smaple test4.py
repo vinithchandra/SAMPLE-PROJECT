bc78c7bcc597141f9678c058975b44d845ed55a1
vinithchandra <111564110+vinithchandra@users.noreply.github.com>
2024-04-03 15:40:24 +0530

    Update smaple test4.py

--- smaple test4.py
+++ smaple test4.py
@@ -3,7 +3,7 @@ from selenium.webdriver.chrome.service import Service
 import time
 
 # Start the WebDriver and open the HTML page
-service = Service(executable_path='C:/usr/local/bin/chromedriver')
+service = Service(executable_path='/usr/local/bin/chromedriver')
 options = webdriver.ChromeOptions()
 options.add_argument("--headless")
 options.add_argument('--no-sandbox')
