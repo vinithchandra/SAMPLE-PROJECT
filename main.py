4af8fa8e9ea37f42fb5cc469d68e04235c5157b5
vinithchandra <111564110+vinithchandra@users.noreply.github.com>
2024-04-05 14:49:39 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+import time
+
+# Start the WebDriver and open the HTML page
+service = Service(executable_path='C:\\chromedriver-win64\\chromedriver.exe')
+options = webdriver.ChromeOptions()
+options.add_argument("--headless")
+options.add_argument('--no-sandbox')
+driver = webdriver.Chrome(service=service, options=options)
+
+# Open the webpage
+driver.get("https://vinithchandra.github.io/SAMPLE-PROJECT/")
+
+# Wait for the webpage to load
+print("Waiting for the webpage to load...")
+time.sleep(5)
+
+# Check the webpage title
+print("Checking the webpage title...")
+assert "Sample Project" in driver.title
+
+# Print the webpage title
+#print("Webpage title:", driver.title)
+
+# Take a screenshot
+timestamp = time.strftime("%Y%m%d-%H%M%S")
+screenshot_file = f"screenshot_{timestamp}.png"
+driver.save_screenshot(screenshot_file)
+#print(f"Screenshot saved to {screenshot_file}")
+
+# Close the WebDriver
+driver.close()
+#print("WebDriver closed.")
